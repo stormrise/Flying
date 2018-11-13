7708ae1b072e21aeba9cd47c959c5c00f8bc4304
stormrise <lilingxiao0414@gmail.com>
2018-11-13 13:24:28 +0800

    fork init

---
+++ desktop/src/hk/hku/cs/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package hk.hku.cs.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import hk.hku.cs.game.Flying;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Flying(), config);
+	}
+}
